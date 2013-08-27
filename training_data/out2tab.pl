7e3cc551d7916012c57f490e2b8e5bdb979a2ce4
loop <loop@192.168.1.64>
2013-08-27 03:05:57 +0100

    Training data table / graph

--- training_data/out2tab.pl
+++ training_data/out2tab.pl
@@ -1,4 +1,5 @@
 #!/usr/bin/perl -w
+use Data::Dumper;
 
 my $file = $ARGV[0] or die "No input file\n";
 
@@ -6,9 +7,20 @@ my $gen = 0;
 
 my @generations;
 
+my $k = 0;
+while ($k < 97) {
+  my $l = 0;
+  push(@generations, [{fitness=>0, stack=>0}]);
+  while ($l < 99) {
+    push(@{$generations[$k]}, { fitness => 0, stack => 0 });
+    $l++;
+  }
+  $k++;
+}
+
 open FILE, $file or die "open failed\n";
 
-while (<>) {
+while (<FILE>) {
   if (/^\s*eval\s*(\d+):\s*([\d\.]+)\s*\[(\d+)\]\s*$/) {
     $generations[$gen]->[$1 - 1]->{fitness} = $2;
     $generations[$gen]->[$1 - 1]->{stack} = $3;
@@ -19,16 +31,16 @@ while (<>) {
 }
 
 my $i = 0;
-while ($i < scalar(@$generations[0])) {
+while ($i < scalar(@{$generations[0]})) {
   my $j = 0;
   while ($j < scalar(@generations)) {
-    print $generations[$j]->[$i];
+    print $generations[$j]->[$i]->{fitness};
     if ($j < scalar(@generations) - 1) {
-      print ',';
+      print ",";
     }
-    print "\n";
     $j++;
   }
+  print "\n";
   $i++;
 }
 
